5bbf8f8fcd3ac0f4a30d9bfeb1542e1360df04b0
Yalchan Niyaziev <projectsyhn@gmail.com>
2024-06-10 14:30:58 +0300

    Create initial project structure

---
+++ src/main/java/com/yalco/servicestoreserviceapi/ServiceStoreServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.yalco.servicestoreserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStoreServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceStoreServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yalco/servicestoreserviceapi/ServiceStoreServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yalco.servicestoreserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStoreServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
